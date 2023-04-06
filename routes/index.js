74c2da573a8eacc0e6aabe845725d496806b6e78
ashwanikumarsingh04 <singhashwani1804@gmail.com>
2023-04-06 20:47:37 +0100

    CRUD updated

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,57 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+router.get("/api/get", function (req, res, next)  {
+  const sqlGet = "SELECT * FROM crud";
+  db.query(sqlGet, (error, result) => {
+    res.send(result);
+  });
+});
+
+router.post("/api/post", function (req, res, next) {
+  const { name, email, contact } = req.body;
+  const sqlInsert =
+    "INSERT INTO crud (name, email, contact) VALUES (?, ?, ?)";
+  db.query(sqlInsert, [name, email, contact], (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
+
+router.delete("/api/remove/:id", function(req, res, next) {
+  const { id } = req.params;
+  const sqlRemove = "DELETE FROM crud WHERE id = ?";
+  db.query(sqlRemove, id, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
+
+router.get("/api/get/:id", function(req, res, next) {
+  const { id } = req.params;
+  const sqlGet = "SELECT * FROM crud WHERE id = ?";
+  db.query(sqlGet, id, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+    res.send(result);
+  });
+});
+
+router.put("/api/update/:id", function(req, res, next)  {
+    const { id } = req.params;
+    const {name, email, contact} = req.body;
+    const sqlUpdate = "UPDATE crud SET name = ?, email = ?, contact = ? WHERE id = ?";
+    db.query(sqlUpdate, [name, email, contact, id], (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+      res.send(result);
+    });
+  });
+
 // all medcine
 router.get('/medicine', function(req, res, next) {
   try {
